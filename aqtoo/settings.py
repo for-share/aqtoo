83f03a43599c9fa326b94fbaf709c8360a19b78c
Bogdan <bogdan.gm24@gmail.com>
2016-07-08 16:11:22 +0300

    use lib for phone field

--- aqtoo/settings.py
+++ aqtoo/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'person_auth',
+    'phonenumber_field',
 ]
 
 MIDDLEWARE_CLASSES = [
--- person_auth/models.py
+++ person_auth/models.py
@@ -1,15 +1,16 @@
 from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 # Create your models here.
 
 class Number(models.Model):
     user = models.ForeignKey(User)
-    phone_regex = RegexValidator(regex=r'\+?\d{20}$',
-                                 message="Number must consists of + and numbers (not more than 20 number)")
-    phone_number = models.CharField(validators=[phone_regex], blank=True, max_length=13, verbose_name="Номер телефону")
+    phone_number = PhoneNumberField(max_length=20)
+    date_create = models.DateTimeField(auto_now_add=True)
+    date_of_processing = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return self.phone_number

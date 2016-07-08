41f286b5daadbb478d3cb7ec90cba930166bf3d9
Bogdan <bogdan.gm24@gmail.com>
2016-07-08 15:37:54 +0300

    change regex

--- person_auth/models.py
+++ person_auth/models.py
@@ -7,8 +7,8 @@ from django.db import models
 
 class Number(models.Model):
     user = models.ForeignKey(User)
-    phone_regex = RegexValidator(regex=r'\+?\d{11}$',
-                                 message="Номеру повинен бути виду +380000000000.")
+    phone_regex = RegexValidator(regex=r'\+?\d{20}$',
+                                 message="Number must consists of + and numbers (not more than 20 number)")
     phone_number = models.CharField(validators=[phone_regex], blank=True, max_length=13, verbose_name="Номер телефону")
 
     def __str__(self):
